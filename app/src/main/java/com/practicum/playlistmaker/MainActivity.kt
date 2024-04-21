277207919a44cb179608cae029e93729eb9be1b8
harmgehog <polstia@gmail.com>
2024-04-21 10:49:35 +0300

    Темы, настройки, поиск, сохранение статуса.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val btnFirst: Button = findViewById(R.id.search_button)
-        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View) {
-                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
-            }
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        btnSearch.setOnClickListener {
+            //Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.searchBtn_text),Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
         }
-        btnFirst.setOnClickListener(buttonClickListener)
-
-        findViewById<Button>(R.id.media_button).setOnClickListener {
-            startActivity(Intent(this, MediaActivity::class.java))
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+        val btnLibraryClickListener: View.OnClickListener = View.OnClickListener {
+            //Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.libraryBtn_text),Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
         }
+        btnLibrary.setOnClickListener(btnLibraryClickListener)
+
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+        btnSettings.setOnClickListener {
+            //Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.settingsBtn_text),Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-    fun onBtnPress(v: View) = startActivity(Intent(this, SettingsActivity::class.java))
-
 }
--- app/src/main/java/com/practicum/playlistmaker/MediaActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MediaActivity.kt
@@ -1,15 +1,19 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 
 class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_media)
-    }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
 
-    fun onBackPress(v: View) = onBackPressedDispatcher.onBackPressed()
+    }
 }
